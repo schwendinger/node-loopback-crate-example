e474d1c87c614e5a00d136e3c2d71fde90e8274a
Manfred Schwendinger <manfredwired@gmail.com>
2015-01-29 22:46:03 +0100

    started to implement delete

--- client/js/app.js
+++ client/js/app.js
@@ -2,15 +2,20 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'lbServices',
-  'myApp.directives',
-  'myApp.controllers'
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'lbServices',
+    'myApp.directives',
+    'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/userlist', {templateUrl: 'partials/userlist.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/newuser', {templateUrl: 'partials/newuser.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/userlist',
+                            {templateUrl: 'partials/userlist.html',
+                             controller: 'UserlistCtrl'});
+        $routeProvider.when('/newuser',
+                            {templateUrl:
+                             'partials/newuser.html',
+                             controller: 'NewuserCtrl'});
+        $routeProvider.otherwise({redirectTo: '/userlist'});
+    }]);
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -3,10 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('MyCtrl1', ['$scope', 'Person', function($scope, Person) {
+    .controller('UserlistCtrl', ['$scope', 'Person', function($scope, Person) {
         $scope.personsCount = Person.count(); // add loopback model
         $scope.persons = Person.find();
+        $scope.deletePerson = function(person){
+            console.log("deletePerson", person);
+        }
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+    .controller('NewuserCtrl', ['$scope', 'Person', function($scope, Person) {
+        $scope.master = {};
+        $scope.save = function(user) {
+            $scope.master = angular.copy(user);
+            $scope.person = Person.create(user);
+            $scope.person.$save();
+        };
+        $scope.reset = function() {
+            $scope.user = angular.copy($scope.master);
+        };
+        $scope.reset();
   }]);
