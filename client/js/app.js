a2b1e2dfdadf649b059305fa9e06832d3392ea6d
Manfred Schwendinger <manfredwired@gmail.com>
2015-01-29 21:26:36 +0100

    implemented userlist

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
@@ -11,7 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/userlist', {templateUrl: 'partials/userlist.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/newuser', {templateUrl: 'partials/newuser.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -4,7 +4,8 @@
 
 angular.module('myApp.controllers', [])
     .controller('MyCtrl1', ['$scope', 'Person', function($scope, Person) {
-        $scope.persons = Person.count(); // add loopback model
+        $scope.personsCount = Person.count(); // add loopback model
+        $scope.persons = Person.find();
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
